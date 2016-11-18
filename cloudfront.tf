57c5cb11c91181d1a372a0bfa7d961fc0af795f8
dilshad <dilshad@qburst.com>
2016-11-18 12:19:52 +0530

    updated s3 and cloudfront configs - working

--- cloudfront.tf
+++ cloudfront.tf
@@ -1,58 +1,59 @@
-resource "aws_cloudfront_distribution" "s3_distribution" {
-  origin {
-    domain_name = "${aws_s3_bucket}.s3-website-ap-northeast-1.amazonaws.com"
-    origin_id   = "origin-bucket-${aws_s3_bucket.website_bucket.id}"
+resource "aws_cloudfront_distribution" "website_cdn" {
+  enabled = true
+  price_class = "PriceClass_200"
+  http_version = "http1.1"
 
-    # s3_origin_config {
-    #   origin_access_identity = "origin-access-identity/cloudfront/ABCDEFG1234567"
+  "origin" {
+    origin_id = "origin-bucket-${aws_s3_bucket.website_bucket.id}"
+    domain_name = "${aws_s3_bucket.website_bucket.website_endpoint}"
+    custom_origin_config {
+      origin_protocol_policy = "http-only"
+      http_port = "80"
+      https_port = "443"
+      origin_ssl_protocols = ["TLSv1"]
+    }
+    # custom_header {
+    #   name = "User-Agent"
+    #   value = "${var.duplicate-content-penalty-secret}"
     # }
   }
-
-  enabled             = true
-  comment             = "Some comment"
   default_root_object = "index.html"
-
-  # logging_config {
-  #   include_cookies = false
-  #   bucket          = "mylogs.s3.amazonaws.com"
-  #   prefix          = "myprefix"
+  # custom_error_response {
+  #   error_code = "404"
+  #   error_caching_min_ttl = "360"
+  #   response_code = "200"
+  #   response_page_path = "${var.not-found-response-path}"
   # }
-
-  # aliases = ["${var.domain}"]
-
-  default_cache_behavior {
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "origin-bucket-${aws_s3_bucket.website_bucket.id}"
-
-    forwarded_values {
+  "default_cache_behavior" {
+    allowed_methods = ["GET", "HEAD", "DELETE", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods = ["GET", "HEAD"]
+    "forwarded_values" {
       query_string = false
-
       cookies {
         forward = "none"
       }
     }
-
+    min_ttl = "0"
+    default_ttl = "300" //3600
+    max_ttl = "1200" //86400
+    target_origin_id = "origin-bucket-${aws_s3_bucket.website_bucket.id}"
+    # // This redirects any HTTP request to HTTPS. Security first!
     viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
+    # compress = true
   }
-
-  price_class = "PriceClass_200"
-
-  restrictions {
-    geo_restriction {
+  "restrictions" {
+    "geo_restriction" {
       restriction_type = "none"
-      # locations        = ["US", "CA", "GB", "DE"]
     }
   }
-
-  tags {
-    Environment = "production"
-  }
+  # "viewer_certificate" {
+  #   acm_certificate_arn = "${var.acm-certificate-arn}"
+  #   ssl_support_method = "sni-only"
+  #   minimum_protocol_version = "TLSv1"
+  # }
 
   viewer_certificate {
     cloudfront_default_certificate = true
   }
+  # aliases = ["${var.domain}"]
 }
--- s3.tf
+++ s3.tf
@@ -3,28 +3,33 @@ provider "aws" {
     region = "ap-northeast-1"
 }
 
-resource "aws_s3_bucket" "website_bucket" {
-    # provider = "aws.${var.region}"
-    provider = "aws.tokyo"
-    bucket = "${var.aws_s3_bucket_name}"
-    acl = "public-read"
-    policy = "${data.template_file.bucket_policy.rendered}"
-    force_destroy = true
-
-    tags {
-        Name = "${var.aws_s3_bucket_name}"
-        Environment = "${var.aws_s3_environment}"
-    }
-
-    website {
-        index_document = "index.html"
-        error_document = "error.html"
-    }
-}
-
 data "template_file" "bucket_policy" {
   template = "${file("${path.module}/policy.json")}"
   vars {
     bucket = "${var.aws_s3_bucket_name}"
   }
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  provider = "aws.tokyo"
+  bucket = "${var.aws_s3_bucket_name}"
+  policy = "${data.template_file.bucket_policy.rendered}"
+
+  website {
+    index_document = "index.html"
+    error_document = "404.html"
+    # routing_rules = "${var.routing_rules}"
+  }
+
+  tags {
+    Name = "${var.aws_s3_bucket_name}"
+    Environment = "${var.aws_s3_environment}"
+  }
+
+  force_destroy=true
+
+  # logging {
+  #   target_bucket = "${var.log_bucket}"
+  #   target_prefix = "${var.log_bucket_prefix}"
+  # }
 }
