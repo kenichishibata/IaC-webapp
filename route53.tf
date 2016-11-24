03db2af7ee4edea0a7e6b2dd1101b2f2bf4bd4ae
kenichishibata <kenichi.shibata3123@gmail.com>
2016-11-24 14:28:24 +0900

    added count

--- route53.tf
+++ route53.tf
@@ -11,6 +11,8 @@ resource "aws_route53_record" "s3_record" {
 }
 
 resource "aws_route53_record" "cname_record" {
+	count = "${var.cdn_boolean}"
+	
 	depends_on = ["aws_cloudfront_distribution.s3_distribution"]
 	zone_id = "${var.hosted_zone_id}"
 	name = "${var.pre_tag}.${var.route53_domain_name}"
