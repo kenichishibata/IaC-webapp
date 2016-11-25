c5a2fea777155695095e89e87c54977c68b590a4
Kenichi Shibata <kenichi.shibata@fastretailing.com>
2016-11-25 22:14:14 +0900

    Update for jenkins domain (#7)
    * Update for jenkins domain
    
    * added route 53 alias for cloudfront

--- route53.tf
+++ route53.tf
@@ -10,6 +10,7 @@ resource "aws_route53_record" "s3_record" {
   }
 }
 
+
 resource "aws_route53_record" "https_record" {
 	count = "${var.cdn_boolean}"
 
@@ -21,6 +22,21 @@ resource "aws_route53_record" "https_record" {
 	alias {
 		name = "${aws_cloudfront_distribution.s3_distribution.domain_name}"
 		zone_id = "${aws_cloudfront_distribution.s3_distribution.hosted_zone_id}"
+		evaluate_target_health = false
+	}
+}
+
+resource "aws_route53_record" "cname_jenkins" {
+	count = "${var.cdn_boolean}"
+
+	depends_on = ["null_resource.jenkins-dcos-installation"]
+	zone_id = "${var.hosted_zone_id}"
+	name = "${var.pre_tag}-jenkins.${var.route53_domain_name}"
+	type = "A"
+
+	alias {
+		name = "${aws_cloudfront_distribution.jenkins_distribution.domain_name}"
+		zone_id = "${aws_cloudfront_distribution.jenkins_distribution.hosted_zone_id}"
 		evaluate_target_health = false
 	}
 }
