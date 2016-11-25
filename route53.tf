71a57d8bba8734f9990e0bc6b7d6cc137cdf51c0
kenichishibata <kenichi.shibata3123@gmail.com>
2016-11-25 14:23:59 +0900

    https_record alias instead of cname_record cname

--- route53.tf
+++ route53.tf
@@ -10,13 +10,18 @@ resource "aws_route53_record" "s3_record" {
   }
 }
 
-resource "aws_route53_record" "cname_record" {
+resource "aws_route53_record" "https_record" {
 	count = "${var.cdn_boolean}"
 
 	depends_on = ["aws_cloudfront_distribution.s3_distribution"]
 	zone_id = "${var.hosted_zone_id}"
 	name = "${var.pre_tag}.${var.route53_domain_name}"
-	type = "CNAME"
+	type = "A"
 	ttl = "60"
-	records = ["${aws_cloudfront_distribution.s3_distribution.domain_name}"]
+
+	alias {
+		name = "${aws_cloudfront_distribution.s3_distribution.domain_name}"
+		zone = "${aws_cloudfront_distribution.s3_distribution.hosted_zone_id}"
+		evaluate_target_health = false
+	}
 }
